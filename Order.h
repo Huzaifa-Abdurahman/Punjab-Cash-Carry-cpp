b1b970ff149192f9199ea79332d830431ab74f65
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2025-05-04 02:11:23 -0700

    Update Order.h

--- Order.h
+++ Order.h
@@ -4,4 +4,40 @@
 #include <string>
 using namespace std;
 
+class Order {
+private:
+    string productNames[10];
+    int quantities[10];
+    int itemCount;
+
+public:
+    Order() : itemCount(0) {}
+
+    bool addItem(const string& productName, int quantity) {
+        if (itemCount < 10) {
+            productNames[itemCount] = productName;
+            quantities[itemCount] = quantity;
+            itemCount++;
+            return true;
+        } else {
+            cout << "Cart full, cannot add more items.\n";
+            return false;
+        }
+    }
+
+    void displayOrder() const {
+        if (itemCount == 0) {
+            cout << "Order is empty.\n";
+            return;
+        }
+        cout << "Order Details:\n";
+        for (int i = 0; i < itemCount; ++i) {
+            cout << "- " << productNames[i] << " x " << quantities[i] << endl;
+        }
+    }
+
+    int getItemCount() const {
+        return itemCount;
+    }
+};
 #endif
